c57365110782c334f4063ad73a3a57aedc7993bc
Jack Nash <50455943+JackRNash@users.noreply.github.com>
2019-06-10 13:11:52 -0700

    Added lookup table for alphanum character capacity

--- src/Encode.java
+++ src/Encode.java
@@ -113,4 +113,100 @@ public class Encode {
         return arr;
     }
 
+    /**
+     * Given a version, returns an array with the alphanumeric character capacity of the error correcting levels
+     * L, M, Q, & H respectively. E.g. alphanumCapacityLookup(1) --> [25, 20, 16, 10]
+     * So the character capacity of version 1 w/ error correction level l is 25
+     * Unfortunately had to be hard coded, no formula exists(as far as I know)
+     * Precondition: 1 <= version <= 40
+     */
+    public static int[] alphanumCapacityLookup(int version) {
+       int[] arr = new int[0];
+
+       switch(version) {
+           case 1:
+               arr = new int[] {25, 20, 16, 10}; break;
+           case 2:
+               arr = new int[] {47, 38, 29, 20}; break;
+           case 3:
+               arr = new int[] {77, 61, 47, 35}; break;
+           case 4:
+               arr = new int[] {114, 90, 67, 50}; break;
+           case 5:
+               arr = new int[] {154, 122, 87, 64}; break;
+           case 6:
+               arr = new int[] {195, 154, 108, 84}; break;
+           case 7:
+               arr = new int[] {224, 178, 125, 93}; break;
+           case 8:
+               arr = new int[] {279, 221, 157, 122}; break;
+           case 9:
+               arr = new int[] {335, 262, 189, 143}; break;
+           case 10:
+               arr = new int[] {395, 311, 221, 174}; break;
+           case 11:
+               arr = new int[] {468, 366, 259, 200}; break;
+           case 12:
+               arr = new int[] {535, 419, 296, 227}; break;
+           case 13:
+               arr = new int[] {619, 483, 352, 259}; break;
+           case 14:
+               arr = new int[] {667, 528, 376, 283}; break;
+           case 15:
+               arr = new int[] {758, 600, 426, 321}; break;
+           case 16:
+               arr = new int[] {854, 656, 470, 365}; break;
+           case 17:
+               arr = new int[] {938, 734, 531, 408}; break;
+           case 18:
+               arr = new int[] {1046, 816, 574, 452}; break;
+           case 19:
+               arr = new int[] {1153, 909, 644, 493}; break;
+           case 20:
+               arr = new int[] {1249, 970, 702, 557}; break; //
+           case 21:
+               arr = new int[] {1352, 1035, 742, 587}; break;
+           case 22:
+               arr = new int[] {1460, 1134, 823, 640}; break;
+           case 23:
+               arr = new int[] {1588, 1248, 890, 672}; break;
+           case 24:
+               arr = new int[] {1704, 1326, 963, 744}; break;
+           case 25:
+               arr = new int[] {1853, 1451, 1041, 779}; break;
+           case 26:
+               arr = new int[] {1990, 1542, 1094, 864}; break;
+           case 27:
+               arr = new int[] {2132, 1637, 1172, 910}; break;
+           case 28:
+               arr = new int[] {2223, 1732, 1263, 958}; break;
+           case 29:
+               arr = new int[] {2369, 1839, 1322, 1016}; break;
+           case 30:
+               arr = new int[] {2520, 1994, 1429, 1080}; break;
+           case 31:
+               arr = new int[] {2677, 2113, 1499, 1150}; break;
+           case 32:
+               arr = new int[] {2840, 2238, 1618, 1226}; break;
+           case 33:
+               arr = new int[] {3009, 2369, 1700, 1307}; break;
+           case 34:
+               arr = new int[] {3183, 2506, 1787, 1394}; break;
+           case 35:
+               arr = new int[] {3351, 2632, 1867, 1431}; break;
+           case 36:
+               arr = new int[] {3537, 2780, 1966, 1530}; break;
+           case 37:
+               arr = new int[] {3729, 2894, 2071, 1591}; break;
+           case 38:
+               arr = new int[] {3927, 3054, 2181, 1658}; break;
+           case 39:
+               arr = new int[] {4087, 3220, 2298, 1774}; break;
+           case 40:
+               arr = new int[] {4296, 3391, 2420, 1852}; break;
+       }
+
+       return arr;
+    }
+
 }
